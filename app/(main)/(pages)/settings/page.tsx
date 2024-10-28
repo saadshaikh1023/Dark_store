fa5337b4091206f940dda257550a4e8f0c1f7e79
Saad Shaikh <73577526+saadshaikh1023@users.noreply.github.com>
2024-10-29 00:07:39 +0530

    Update page.tsx

--- app/(main)/(pages)/settings/page.tsx
+++ app/(main)/(pages)/settings/page.tsx
@@ -1,13 +1,14 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 'use client';
 /* eslint-disable @typescript-eslint/no-empty-object-type */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useState, useEffect, Key } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ref, onValue } from 'firebase/database';
 import { database } from '@/firebase'; // Make sure this path is correct
 import data from '@/data.json'; // Import the warehouse data
 
 type WarehouseData = {
-  id: Key | null | undefined;
+  id: string;
   darkStoreName: string;
   warehouseSize: string;
   ceilingHeight: string;
@@ -39,10 +40,13 @@ const Settings = (props: Props) => {
     onValue(warehousesRef, (snapshot) => {
       const data = snapshot.val();
       if (data) {
-        const warehouseList = Object.entries(data).map(([key, value]) => ({
-          ...value as WarehouseData,
-          id: key, // Assign 'key' as 'id'
-        }));
+        const warehouseList = Object.entries(data).map(([key, value]) => {
+          const warehouseData = value as WarehouseData;
+          return {
+            ...warehouseData,
+            id: key, // Only set 'id' if it doesn't exist in warehouseData
+          };
+        });
         setWarehouses(warehouseList);
       }
     });
